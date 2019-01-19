fe957ffefeb4b92c311e952ef7049e1f945a1932
Mahmoud5Saad <43937177+Mahmoud5Saad@users.noreply.github.com>
2019-01-19 19:44:12 +0200

    Update pipeline.py
    fix spelling mistakes

--- pipeline.py
+++ pipeline.py
@@ -13,7 +13,7 @@ Row=float(input('Density: '))
 Meu=float(input('Absolute viscosity: '))
 D=float(input('Inner diameter: '))
 E=float(input('Absolute roughness: '))
-L=float(input('Pipe lenght: '))
+L=float(input('Pipe length: '))
 Hp=float(input('Pump head: '))
 K=float(input('Friction coefficient: '))
 P1=float(input('Pressure at point 1 : '))
@@ -31,16 +31,16 @@ e=2.718281828
 #calculations expressions
 In=int(input('\n\nNow we have 2 options:\nTo calculate Delta_Z please write 1 and press ENTER \nTo calculate Q  please write 2 and press ENTER\n\n') )
 
-#the code for calculationg Delta_Z
+#the code for calculating Delta_Z
 if In==1:
-    B=float(input('\n\nPlease enter Volume flowrate in m^3/hr:'))
+    B=float(input('\n\nPlease enter Volume flowrate in m^3/hr: '))
     m=3600
     Q=B/m
     V=4*Q/(Pie*math.pow(D,2))
     Re=(Row*V*D)/Meu
     in_Ln=0.27*(E/D)+5.74*( math.pow((1/Re),0.9) )
     F=1.325*( math.pow(math.log(in_Ln),-2) )
-    print('Fricition factor= '),(F)
+    print('Friction factor= '),(F)
     left=(F*L/D) + K
     right=math.pow(V,2) / (2*g)
     Hl=left*right
@@ -66,7 +66,7 @@ else :
             diff=(F2-F1)*-1
         if diff <= 0.001 :
             print('\n\nVolume flow rate     = '),(Q*3600)
-            print('New Fricition factor = '),(F2)
+            print('New Friction factor = '),(F2)
             print('Velocity             = '),(V)
             break
         else:
